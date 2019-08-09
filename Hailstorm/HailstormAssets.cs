93144119ceb8caa9691529498ff3029020edaf21
Jarlyk <53195893+Jarlyk@users.noreply.github.com>
2019-08-08 20:31:59 -0700

    - Updated Hailstorm to use AssetPlus instead of SoundPlus - Added dependencies to Hailstorm manifest

--- Hailstorm/HailstormAssets.cs
+++ Hailstorm/HailstormAssets.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using System.Text;
-using SoundPlus;
+using AssetPlus;
 using UnityEngine;
 
 namespace JarlykMods.Hailstorm
